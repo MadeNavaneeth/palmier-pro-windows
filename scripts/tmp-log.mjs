9b215dad6740dc553dac0aaf9ead67b40b101f0e
MadeNavaneeth <151734681+MadeNavaneeth@users.noreply.github.com>
2026-08-23 12:50:08 +0530

    chore: remove temporary log helper

--- scripts/tmp-log.mjs
+++
@@ -1,4 +0,0 @@
-import fs from 'node:fs';
-const raw = fs.readFileSync(process.env.TEMP + '\\ap.log', 'utf8').replace(/\x1B\[[0-9;]*m/g, '');
-const i = raw.indexOf('AssertionError');
-console.log(raw.slice(i, i + 900));
